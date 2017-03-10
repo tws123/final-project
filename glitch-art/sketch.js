df9eeef15ec06a0934a8742389c185d87cff3eca
tws123 <tesstify@gmail.com>
2017-03-09 23:43:05 -0800

    fixed slider incrementation and updated code to match most of Susan's suggestions

--- glitch-art/sketch.js
+++ glitch-art/sketch.js
@@ -10,7 +10,7 @@
 
 */
 
-//var img1;
+
 var marginTop = 110;
 var marginSide = 250;
 //var filter = false;
@@ -18,11 +18,14 @@ var grid; // size of square that grid is divided by
 var slider;
 var imgS = 75; // small image size
 var imgB = 500; // large image size
+var portraitsLg = [];
+var portraitsSm = [];
 
 
 
 
 function preload() {
+
 
   img1 = loadImage('../images/friedaWeb.jpg'); // larger photo 
   img2 = loadImage('../images/mandelaWeb.jpg'); // larger photo 
@@ -44,25 +47,25 @@ function preload() {
 
   img16 = loadImage('../images/chuckItRed.png'); // logo for landing page (may choose to use logo w/ description instead)
 
-
+  portraitsLg = new Array(img1, img2, img3, img4, img5, img6);
+  portraitsSm = new Array(img7, img8, img9, img10, img11, img12);
 }
 
 function setup() {
-  var imgBArray = new Array(img1, img2, img3, img4, img5, img6);
-  var imgSArray = new Array(img7, img8, img9, img10, img11, img12);
 
   createCanvas(1000, 720);
   imageMode(CENTER);
   background(245, 245, 245);
-  image(imgSArray[0], width / 5 + 2, marginTop + imgS/2, imgS, imgS);
-  image(imgSArray[1], width / 5 + 2, marginTop + imgS/2 + imgS + 10, imgS, imgS); //123
-  image(imgSArray[2], width / 5 + 2, marginTop + imgS/2 + (2 * imgS) + 20, imgS, imgS); //208
-  image(imgSArray[3], width / 5 + 2, marginTop + imgS/2 + (3 * imgS) + 30, imgS, imgS); //293
-  image(imgSArray[4], width / 5 + 2, marginTop + imgS/2 + (4 * imgS) + 40, imgS, imgS); //378
-  image(imgSArray[5], width / 5 + 2, marginTop + imgS/2 + (5 * imgS) + 50, imgS, imgS); //463
+  image(portraitsSm[0], width / 5 + 2, marginTop + imgS / 2, imgS, imgS);
+  image(portraitsSm[1], width / 5 + 2, marginTop + imgS / 2 + imgS + 10, imgS, imgS); //123
+  image(portraitsSm[2], width / 5 + 2, marginTop + imgS / 2 + (2 * imgS) + 20, imgS, imgS); //208
+  image(portraitsSm[3], width / 5 + 2, marginTop + imgS / 2 + (3 * imgS) + 30, imgS, imgS); //293
+  image(portraitsSm[4], width / 5 + 2, marginTop + imgS / 2 + (4 * imgS) + 40, imgS, imgS); //378
+  image(portraitsSm[5], width / 5 + 2, marginTop + imgS / 2 + (5 * imgS) + 50, imgS, imgS); //463
   // image(img12, imgS, imgS);
+
   image(img1, width / 2, height / 2, imgB, imgB); // main image
-  slider = createSlider(10, 50, 20, 10); // (min, max, [default value], [step]) 
+  slider = createSlider(0, 50, 25, 25); // (min, max, [default value], [step]) 
   slider.position(width - marginSide + 50, marginTop + 80);
   textAlign(CENTER, CENTER);
   // fill(0);
@@ -73,7 +76,7 @@ function setup() {
 function draw() {
   fill(0);
   // text("abstraction", width - marginSide + 120, marginTop + 120);
-  grid = slider.value();
+  grid = max(10, slider.value()); // allows slider to start at 0 and increment by multiples of 25 w/out sq being too small.
   filterBoxes(width - marginSide + 50, marginTop); // draw button 
   ellipseMode(CENTER);
   rectMode(CENTER);
@@ -89,34 +92,35 @@ function mousePressed() {
 
   if (mouseX > width - marginSide + 50 && mouseX < width - marginSide + 50 + 130 && mouseY > marginTop && mouseY < marginTop + 40) { // checks if mouse is within buttom dimensions
     //page = (page + 1) % 6; // switches pages
+    //background(255);
     chuckIt();
   }
+}
+
+function sidePanel() {
+  
 }
 
 function chuckIt() {
   ellipseMode(CENTER);
   rectMode(CENTER);
 
-  var a;
-  var b;
-  var c;
-  var d;
 
   for (i = 0; i < 500 / grid; i++) {
     for (j = 0; j < 500 / grid; j++) {
-      a = img1.get(random(grid * i, min(grid * i + grid, 500)), random(grid * j, min(grid * j + grid, 500)));
-      b = img1.get(random(grid * i, min(grid * i + grid, 500)), random(grid * j, min(grid * j + grid, 500)));
-      c = img1.get(random(grid * i, min(grid * i + grid, 500)), random(grid * j, min(grid * j + grid, 500)));
-      d = img1.get(random(grid * i, min(grid * i + grid, 500)), random(grid * j, min(grid * j + grid, 500)));
+      var color1 = img1.get(random(grid * i, min(grid * i + grid, 500)), random(grid * j, min(grid * j + grid, 500)));
+      var color2 = img1.get(random(grid * i, min(grid * i + grid, 500)), random(grid * j, min(grid * j + grid, 500)));
+      var color3 = img1.get(random(grid * i, min(grid * i + grid, 500)), random(grid * j, min(grid * j + grid, 500)));
+      var color4 = img1.get(random(grid * i, min(grid * i + grid, 500)), random(grid * j, min(grid * j + grid, 500)));
 
       noStroke();
-      fill(a);
+      fill(color1);
       rect(250 + grid / 2 + i * grid, 110 + grid / 2 + j * grid, grid, grid);
-      fill(b);
+      fill(color2);
       ellipse(250 + grid / 2 + i * grid, 110 + grid / 2 + j * grid, grid - 5, grid - 5);
-      fill(c);
+      fill(color3);
       ellipse(250 + grid / 2 + i * grid, 110 + grid / 2 + j * grid, grid - 10, grid - 10);
-      fill(d);
+      fill(color4);
       ellipse(250 + grid / 2 + i * grid, 110 + grid / 2 + j * grid, grid - 15, grid - 15);
     }
   }
